4d4039f1d496f7994d37e39692d4c7e1eef6099b
vagrant <vagrant@localhost.localdomain>
2020-10-26 11:26:32 +0000

    API3

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,13 +1,19 @@
 class OmniauthCallbacksController < ApplicationController
-  def google
-    @user = User.find_for_google(request.env['omniauth.auth'])
+  def google_oauth2
+    callback_for(:google)
+  end
 
+  def callback_for(provider)
+    @user = User.find_for_google(request.env["omniauth.auth"])
     if @user.persisted?
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
       sign_in_and_redirect @user, event: :authentication
     else
-      session['devise.google_data'] = request.env['omniauth.auth']
+      session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
       redirect_to new_user_registration_url
     end
+  end
+
+  def failure
+    redirect_to root_path
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_090809) do
+ActiveRecord::Schema.define(version: 2020_10_26_094713) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -70,6 +70,8 @@ ActiveRecord::Schema.define(version: 2020_10_23_090809) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
+    t.string "token"
+    t.string "meta"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
